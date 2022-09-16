28bd072777f986bcfe058db93d60db3804a74e4f
Yoon Hwa Jeong <yjeong0114@hotmail.com>
2022-09-15 21:53:58 -0500

    first commit

---
+++ app/api/routes.py
@@ -0,0 +1,5 @@
+from flask import Blueprint, request, jsonify, render_template
+from helpers import token_required
+from models import db, User, Contact, contact_schema, contacts_schema
+
+api = Blueprint('api',__name__, url_prefix='/api')
